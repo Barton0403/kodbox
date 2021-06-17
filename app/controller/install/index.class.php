e8dd506c74b1bccd1b3684fbceeafb9774dfff95
barton <barton.li@hr-mp.com>
2021-06-17 17:21:35 +0800

    doc: 添加mysql端口配置、redis密码配置
    由于框架问题，这些配置了目前也无法正常使用

--- app/controller/install/index.class.php
+++ app/controller/install/index.class.php
@@ -261,6 +261,10 @@ class installIndex extends Controller {
                 $port = Input::get("{$cacheType}Port", 'require');
                 if($cacheType == 'redis') {
                     $conn = $handle->connect($host, $port, 1);
+                    $password = Input::get("{$cacheType}Pwd") ?: '';
+                    if ($password) {
+                        $handle->auth($password);
+                    }
                 }else{
                     $conn = $handle->addServer($host, $port);
                 }
@@ -311,6 +315,7 @@ class installIndex extends Controller {
         if(isset($host) && isset($port)){
             $text[] = "\$config['cache']['{$cacheType}']['host'] = '{$host}';";
             $text[] = "\$config['cache']['{$cacheType}']['port'] = '{$port}';";
+            $text[] = "\$config['cache']['{$cacheType}']['password'] = '{$password}';";
         }
         $file = $this->userSetting;
         if(!@file_exists($file)) @touch($file);
